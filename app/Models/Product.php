004122ba38c235cda2b12f7662a621c65286a217
Abdessalem <insloumach@gmail.com>
2025-04-29 13:31:06 +0200

    database conception

---
+++ app/Models/Product.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'name_ar',
+        'name_en',
+        'slug',
+        'description_ar',
+        'description_en',
+        'price',
+        'old_price',
+        'quantity',
+        'rating',
+        'status',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
---
+++ database/migrations/2025_04_29_093501_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete();
+            $table->string('name_ar');
+            $table->string('name_en');
+            $table->string('slug')->unique();
+            $table->text('description_ar')->nullable();
+            $table->text('description_en')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->decimal('old_price', 10, 2)->nullable();
+            $table->integer('quantity')->default(0);
+            $table->float('rating')->default(0);
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
